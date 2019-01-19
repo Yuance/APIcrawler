1820eb25e864392fea962fb68f6d35b451bda688
yc <yuancez@gmail.com>
2019-01-19 17:08:42 +0800

    slave pipeline

--- apicrawler/apicrawler/pipelines.py
+++ apicrawler/apicrawler/pipelines.py
@@ -19,24 +19,25 @@ class ApicrawlerPipeline(object):
 class MongoDBPipeline(object):
 
     def __init__(self):
-        connection = pymongo.MongoClient(
+        self.connection = pymongo.MongoClient(
             settings['MONGODB_SERVER'],
             settings['MONGODB_PORT']
         )
-        db = connection[settings['MONGODB_DB']]
+        db = self.connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
-        valid = True
         for data in item:
             if not data:
-                valid = False
                 raise DropItem("Missing {0}!".format(data))
-        if valid:
-            self.collection.insert(dict(item))
-            log.msg("Match added to MongoDB database!",
-                    level=log.DEBUG, spider=spider)
+
+        self.collection.insert(dict(item))
+        log.msg("Match added to MongoDB database!", level=log.DEBUG, spider=spider)
         return item
+
+    def close_spider(self, spider):
+        self.connection.close()
+
 
 
 
